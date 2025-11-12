5f0d968367a972534a524db200beee95e0ba857f
Mathis <mathisdumage0@gmail.com>
2025-11-12 18:23:14 +0100

    First commit

---
+++ Backend/mail-service/src/test/java/com/project/Prune/PruneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Prune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
